dd34ff8f338d4d28039f1a3d6b6735553f800b4d
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-08-02 14:38:02 +0600

    workpace route initiate

--- controllers/workspaceController.js
+++ controllers/workspaceController.js
@@ -2,65 +2,54 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ztan.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const store = {
-    handle: [],
-    setHandle: function (data) { this.handle = data }
-}
 
 async function run() {
-    // let add;
-    // try {
-    //     await client.connect()
-    //     const workspaceCollection = client.db("pro-man").collection("workspace");
-
-    //     exports.addworkspace = async function (req, res) {
-    //         res.send('success')
-    //     }
 
 
-    // }
-    // finally {
+    const addWorkspace = () => {
+        try {
+            await client.connect()
+            const workspaceCollection = client.db("pro-man").collection("workspace");
 
-    // }
-    // store.setHandle(add)
-}
-run().catch(console.dir);
-// const getWorkspaces = () => {
-//     try {
-//         await client.connect()
-//         const workspaceCollection = client.db("pro-man").collection("workspace");
+        } catch {
 
-//     } catch {
+        }
+    }
+    const getWorkspaces = () => {
+        try {
+            await client.connect()
+            const workspaceCollection = client.db("pro-man").collection("workspace");
 
-//     }
-// }
-// const getWorkspace = () => {
-//     try {
-//         await client.connect()
-//         const workspaceCollection = client.db("pro-man").collection("workspace");
+        } catch {
 
-//     } catch {
+        }
+    }
+    const getWorkspace = () => {
+        try {
+            await client.connect()
+            const workspaceCollection = client.db("pro-man").collection("workspace");
 
-//     }
-// }
-// const updateWorkspace = () => {
-//     try {
-//         await client.connect()
-//         const workspaceCollection = client.db("pro-man").collection("workspace");
+        } catch {
 
-//     } catch {
+        }
+    }
+    const updateWorkspace = () => {
+        try {
+            await client.connect()
+            const workspaceCollection = client.db("pro-man").collection("workspace");
 
-//     }
-// }
-// const deleteWorkspace = () => {
-//     try {
-//         await client.connect()
-//         const workspaceCollection = client.db("pro-man").collection("workspace");
+        } catch {
 
-//     } catch {
+        }
+    }
+    const deleteWorkspace = () => {
+        try {
+            await client.connect()
+            const workspaceCollection = client.db("pro-man").collection("workspace");
 
-//     }
-// }
+        } catch {
 
-// module.exports = { addWorkspace, getWorkspaces, getWorkspace, updateWorkspace, deleteWorkspace }
-// module.exports = { store }
+        }
+    }
+
+    module.exports = { addWorkspace, getWorkspaces, getWorkspace, updateWorkspace, deleteWorkspace }
--- routes/workspaces.js
+++ routes/workspaces.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const workspaceController = require('../controllers/workspaceController')
-// const workspace = workspaceController.store
-// console.log(workspaceController);
+
 
 router.route('/')
-// .get(workspaceController.getWorkspaces)
-// .post(workspace)
-// .patch(workspaceController.updateWorkspace)
-// .delete(workspaceController.deleteWorkspace)
+    .get(workspaceController.getWorkspaces)
+    .post(workspaceController.addWorkspace)
+    .patch(workspaceController.updateWorkspace)
+    .delete(workspaceController.deleteWorkspace)
 
-// router.route('/:id')
-//     .get(workspaceController.getWorkspace);
+router.route('/:id')
+    .get(workspaceController.getWorkspace);
 
 module.exports = router;
