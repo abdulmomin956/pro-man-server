c054d8afca06ce63b3581ceb9b246d14d81cda5f
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-08-30 19:32:56 +0600

    socket io working

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const http=require("http");
+const http = require("http");
 var cookieParser = require("cookie-parser");
-const {Server}=require("socket.io");
+const { Server } = require("socket.io");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-const server=http.createServer(app);
+const server = http.createServer(app);
 
 //all routes
 app.use("/", require("./routes/root"));
@@ -45,26 +45,36 @@ app.use("/profile", require("./routes/profile"));
 app.use("/invite", require("./routes/invite"));
 // socket.io 
 
-const io=new Server(server,{
-  cors:{
-  
-    origin:"http://localhost:3000/chat ",
-    methods:["GET","POST"],
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", "https://pro-man-4cb6f.web.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   }
 });
-io.on("connected",(socket)=>{
-  console.log(`user connected:${socket.id}`);
 
-    socket.to(data.room).emit("receive_mas", data)
- 
+io.on("connection", (socket) => {
+  console.log(`User Connected:${socket.id}`);
 
-  socket.on("disconnect",()=>{
-    console.log("user disconnect")
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    // console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    // console.log("user disconnect", socket.id)
   });
 });
 
 
 
-app.listen(port, () => {
-  console.log("running the server with port " + port);
-});
+// app.listen(port, () => {
+//   console.log("running the server with port " + port);
+// });
+
+server.listen(port, () => {
+  console.log("server is running");
+})
