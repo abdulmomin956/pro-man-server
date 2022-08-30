782252e32c35d4dc13df1b2a3273af4caeec08f7
muzahid37 <muzahidul372709@gmail.com>
2022-08-30 13:42:51 +0600

    add socket

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const http=require("http");
 var cookieParser = require("cookie-parser");
-
+const {Server}=require("socket.io");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+const server=http.createServer(app);
 
 //all routes
 app.use("/", require("./routes/root"));
@@ -41,6 +43,27 @@ app.use("/profile", require("./routes/profile"));
 
 // Invite Workspace member
 app.use("/invite", require("./routes/invite"));
+// socket.io 
+
+const io=new Server(server,{
+  cors:{
+  
+    origin:"http://localhost:3000/chat ",
+    methods:["GET","POST"],
+  }
+});
+io.on("connected",(socket)=>{
+  console.log(`user connected:${socket.id}`);
+
+    socket.to(data.room).emit("receive_mas", data)
+ 
+
+  socket.on("disconnect",()=>{
+    console.log("user disconnect")
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log("running the server with port " + port);
